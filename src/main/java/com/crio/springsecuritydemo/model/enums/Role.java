7bd93f60ce9c6f806a44dbf2cfbd2a76f523e465
jhaaakash <aakashkumarjha295@gmail.com>
2024-10-24 22:40:13 +0530

    Saving the genreated credential in Role Enum File

--- src/main/java/com/crio/springsecuritydemo/model/enums/Role.java
+++ src/main/java/com/crio/springsecuritydemo/model/enums/Role.java
@@ -4,3 +4,46 @@ public enum Role {
     USER,
     ADMIN
 }
+
+/*
+ * 
+ {
+	"name": "Aakash",
+    "email": "Aakashjha@abc.com",
+    "password": "testing",
+    "role": "ADMIN"
+}
+ * 
+ * {
+    "message": "Success",
+    "accessToken": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBYWthc2hqaGFAYWJjLmNvbSIsImlhdCI6MTcyOTc4MzE1OSwiZXhwIjoxNzI5ODY5NTU5fQ.87NblsF5Vlw2P0lq2nDINe6MXmM9JockWfSmNNbLFgc"
+}
+
+=============================================================
+{
+	"name": "Abhishek",
+    "email": "abhishekjha@abc.com@abc.com",
+    "password": "testing",
+    "role": "USER"
+}
+{
+    "message": "Success",
+    "accessToken": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYmhpc2hla2poYUBhYmMuY29tQGFiYy5jb20iLCJpYXQiOjE3Mjk3ODM2MjYsImV4cCI6MTcyOTg3MDAyNn0.ahzNsBjai74-2czclo7KyHg5Uvz0z_3m97yg_NHkOWY"
+}
+
+========================================================
+
+Login:
+
+{
+    "email": "Aakashjha@abc.com",
+    "password": "testing"
+}
+
+{
+    "message": "Success",
+    "accessToken": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBYWthc2hqaGFAYWJjLmNvbSIsImlhdCI6MTcyOTc4NDc2MSwiZXhwIjoxNzI5ODcxMTYxfQ.S8LqNiQ3Ey3CcsVKhUoFcJ5eGv60RfY7mqzPkXfI6v0"
+}
+
+========================================================
+ */
